7b956c50e0966a9bfd8b7aeb9e8438772a140bc9
H2NCH2COOH <wzypublic@gmail.com>
2020-10-25 16:35:19 +0800

    Update main to print error

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ mod lang;
 
 fn main() {
     let mut bytes = std::io::stdin().bytes();
-    let p = lang::parse("STDIN", &mut bytes).unwrap();
-    for ref v in p {
+    let p = lang::parse("STDIN", &mut bytes);
+
+    if let Err(e) = p {
+        println!("{}", e);
+        return;
+    }
+
+    for ref v in p.unwrap() {
         lang::print(&mut std::io::stdout(), v).unwrap();
         std::io::stdout().write(b"\n");
     }
